2ede96457d2555a2b4ea99b1af08f638889b8c24
xudongk <154393425+xudongk@users.noreply.github.com>
2024-01-08 14:42:31 +0800

    Update test.sql

--- sql/test.sql
+++ sql/test.sql
@@ -1,6 +1,15 @@
+START TRANSACTION;
 INSERT INTO test2 (id, name, age, email) VALUES(4, '1', 0, '2');
 INSERT INTO test2 (id, name, age, email) VALUES(5, '1', 0, '2');
 INSERT INTO test2 (id, name, age, email) VALUES(6, '1', 0, '2');
 INSERT INTO test2 (id, name, age, email) VALUES(7, '1', 0, '2');
 INSERT INTO test2 (id, name, age, email) VALUES(8, '1', 0, '2');
 INSERT INTO test2 (id, name, age, email) VALUES(8, '1', 0, '2');
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+BEGIN
+  ROLLBACK;
+  RESIGNAL;
+END;
+
+-- 提交事务
+COMMIT;
