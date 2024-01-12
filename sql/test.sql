ae55c776b0470f1cdbbec08d287282bfd8dc30e9
xudongk <154393425+xudongk@users.noreply.github.com>
2024-01-12 09:56:25 +0800

    Update test.sql

--- sql/test.sql
+++ sql/test.sql
@@ -3,4 +3,4 @@ INSERT INTO test2 (id, name, age, email) VALUES(5, '1', 0, '2');
 INSERT INTO test2 (id, name, age, email) VALUES(6, '1', 0, '2');
 INSERT INTO test2 (id, name, age, email) VALUES(7, '1', 0, '2');
 INSERT INTO test2 (id, name, age, email) VALUES(8, '1', 0, '2');
-INSERT INTO test2 (id, name, age, email) VALUES(8, '1', 0, '2');
+INSERT INTO test2 (id, name, age, email) VALUES(4, '1', 0, '2');
