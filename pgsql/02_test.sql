982734f104c7e543e7cb7b467588a4163426d99f
xudongk <154393425+xudongk@users.noreply.github.com>
2024-01-24 11:47:53 +0800

    Update and rename test.sql to 02_test.sql

---
+++ pgsql/02_test.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.test (id, "name") VALUES(2, '1');
+INSERT INTO public.test (id, "name") VALUES(3, '1');
--- pgsql/test.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public.test (id, "name") VALUES(1, '1');
-INSERT INTO public.test (id, "name") VALUES(1, '1');
