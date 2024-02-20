7ed1ec3864f180536843bdad4b97c9dfda1ae477
xudongk <154393425+xudongk@users.noreply.github.com>
2024-02-20 13:51:54 +0800

    Create 05_combineOpera.sql

---
+++ pgsql/05_combineOpera.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.test (id, "name") VALUES(10, '1testcombine');
+INSERT INTO public.test2 (id, "name") VALUES(20, 'testcombine1');
+update public.test set "name" ='test_combine' where id='1';
+update public.test2 set "name" ='test_combine' where id='8';
+delete from public.test where id='2';
