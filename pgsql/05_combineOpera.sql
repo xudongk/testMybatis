fa88e2c6bc56a09a30af247ccc26d1e6d62aeb91
xudongk <154393425+xudongk@users.noreply.github.com>
2024-02-20 13:57:50 +0800

    Update 05_combineOpera.sql

--- pgsql/05_combineOpera.sql
+++ pgsql/05_combineOpera.sql
@@ -1,4 +1,4 @@
-INSERT INTO public.test (id, "name") VALUES(10, '1testcombine');
+INSERT INTO public.test3 (id, "name") VALUES(10, '1testcombine');
 INSERT INTO public.test2 (id, "name") VALUES(20, 'testcombine1');
 update public.test set "name" ='test_combine' where id='1';
 update public.test2 set "name" ='test_combine' where id='8';
