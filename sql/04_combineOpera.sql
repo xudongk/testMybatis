bea576121ff7df6fd684cbed41622c50e81441a4
xudongk <154393425+xudongk@users.noreply.github.com>
2024-02-20 12:25:38 +0800

    Update 04_combineOpera.sql

--- sql/04_combineOpera.sql
+++ sql/04_combineOpera.sql
@@ -1,6 +1,6 @@
 INSERT INTO test2 (id, name, age, email) VALUES(20, '1', 0, '2');
 INSERT INTO test2 (id, name, age, email) VALUES(30, 'testCombine2', 0, '2');
 INSERT INTO test2 (id, name, age, email) VALUES(40, 'testCombine2', 0, '2');
-update test2 set name='testUpdateCombine1' where id='11';
+update test set name='testUpdateCombine1' where id='11';
 update test2 set name='testUpdateCombine2' where id='12';
 delete from test2 where id='7';
