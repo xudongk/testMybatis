318296361893ebf789af9579554933d01facd580
xudongk <154393425+xudongk@users.noreply.github.com>
2024-02-20 11:34:56 +0800

    Update and rename 01_combineOpera.sql to 00_combineOpera.sql

--- sql/00_combineOpera.sql
+++ sql/00_combineOpera.sql
@@ -3,4 +3,4 @@ INSERT INTO test2 (id, name, age, email) VALUES(30, 'testCombine2', 0, '2');
 INSERT INTO test2 (id, name, age, email) VALUES(40, 'testCombine2', 0, '2');
 update test2 set name='testUpdateCombine1' where id='11';
 update test2 set name='testUpdateCombine2' where id='12';
-delete from test where id='7';
+delete from test2 where id='7';
