6173ec77c56c88bb098efd8899528eb877516192
xudongk <154393425+xudongk@users.noreply.github.com>
2024-02-20 13:01:12 +0800

    Update 01_insertTest.sql

--- pgsql/01_insertTest.sql
+++ pgsql/01_insertTest.sql
@@ -1,4 +1,4 @@
 INSERT INTO public.test (id, "name") VALUES(1, '1');
 INSERT INTO public.test (id, "name") VALUES(4, '1');
 INSERT INTO public.test (id, "name") VALUES(2, '1');
-INSERT INTO public.test3 (id, "name") VALUES(3, '1');
+INSERT INTO public.test (id, "name") VALUES(3, '1');
